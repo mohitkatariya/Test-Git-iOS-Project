7c968926c249f78fa2bb9540fcc96b262794fe27
mohit <mohit@mohits-MacBook-Pro.local>
2014-06-12 19:13:01 +0530

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by mohit  on 12/06/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
