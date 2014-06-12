8e98e7d4af76ae7b6b93d41fdbf4ce59aee16de2
mohit <mohit@mohits-MacBook-Pro.local>
2014-06-12 19:46:26 +0530

    added a test class

---
+++ test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  GitTest
+//
+//  Created by mohit  on 12/06/14.
+//  Copyright (c) 2014 mohit . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  GitTest
+//
+//  Created by mohit  on 12/06/14.
+//  Copyright (c) 2014 mohit . All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
